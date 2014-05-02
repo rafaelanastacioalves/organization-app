369ab0dc8c9de46a786bbe055c7cc8f589dff263
rafaelanastacioalves <rafaelanastacioalves@gmail.com>
2014-05-02 00:17:51 -0300

    Added Calendar Tests Project

---
+++ CalendarReadTest/src/com/example/calendarreadtest/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.calendarreadtest;
+
+import android.view.Menu;
+import java.text.Format;
+import android.app.Activity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements OnClickListener{
+
+	
+	private Cursor mCursor = null;
+	private static final String[] COLS = new String[]
+	{ CalendarContract.Events.TITLE, CalendarContract.Events.DTSTART};
+	
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.activity_main);
+
+
+    	mCursor = getContentResolver().query(
+
+
+    	CalendarContract.Events.CONTENT_URI, COLS, null, null, null
+    	
+    			);
+    	mCursor.moveToFirst();
+
+
+    	Button b = (Button)findViewById(R.id.next);
+
+
+    	b.setOnClickListener(this);
+    	b = (Button)findViewById(R.id.previous);
+
+
+    	b.setOnClickListener(this);
+    	onClick(findViewById(R.id.previous));
+    	
+    	b = (Button)findViewById(R.id.add);
+    	b.setOnClickListener(this);
+    	onClick(findViewById(R.id.add));
+
+
+    	}
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    
+    
+    @Override
+    public void onClick(View v) {
+    TextView tv = (TextView)findViewById(R.id.data);
+    
+    //EditText mv = (EditText)findViewById(R.id.textBox);
+
+
+    String title = "N/A";
+
+
+    Long start = 0L;
+
+
+    switch(v.getId()) {
+    case R.id.next:
+    if(!mCursor.isLast()) mCursor.moveToNext();
+    break;
+    case R.id.previous:
+    if(!mCursor.isFirst()) mCursor.moveToPrevious();
+    break;
+    case R.id.add:
+    addDate();
+    break;
+    }
+
+
+    Format df = DateFormat.getDateFormat(this);
+    Format tf = DateFormat.getTimeFormat(this);
+    
+    
+    
+       
+    try {
+    title = mCursor.getString(0);
+
+    start = mCursor.getLong(1);
+
+    } catch (Exception e) {
+    //ignore
+
+    }
+    tv.setText(title+" on "+df.format(start)+" at "+tf.format(start));
+    }
+    
+    public void addDate(){
+    	
+    }
+    
+}
