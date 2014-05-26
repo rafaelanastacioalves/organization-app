0f7177c7ef5f807d559995064a952b513032dffc
rafaelanastacioalves <rafaelanastacioalves@gmail.com>
2014-05-26 18:48:21 -0300

    Some refactoring on folders and packages
    We no longer need GTDManagerDB and CalendarReadTest

---
+++ Juntando_Tudo/src/ime/pfc/CheckApp/Data/model/Table.java
@@ -0,0 +1,22 @@
+package ime.pfc.CheckApp.Data.model;
+
+public abstract class Table {
+	int id;
+	String created_at;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(String created_at) {
+		this.created_at = created_at;
+	}
+}
---
+++ Juntando_Tudo/src/ime/pfc/CheckApp/Presentation/dialog/ChangeDialog.java
@@ -0,0 +1,71 @@
+package ime.pfc.CheckApp.Presentation.dialog;
+
+import ime.pfc.CheckApp.Business.Controller;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.ArrayAdapter;
+
+import com.juntando_tudo.R;
+
+public class ChangeDialog extends DialogFragment {
+
+	private Controller aController;
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+		aController = (Controller) (getActivity()).getApplicationContext();
+		LayoutInflater liAdd = LayoutInflater.from(getActivity());
+		AlertDialog.Builder builderSingle = new AlertDialog.Builder(
+				getActivity());
+		builderSingle.setIcon(R.drawable.ic_launcher);
+		builderSingle.setTitle(getResources().getString(
+				R.string.change_list_select));
+		final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+				getActivity(), android.R.layout.select_dialog_item);
+		arrayAdapter.addAll(aController.getAllListsName());
+		builderSingle.setNegativeButton(
+				getResources().getString(R.string.Cancel),
+				new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog,
+							int listNamePosition) {
+						dialog.dismiss();
+					}
+				});
+
+		builderSingle.setAdapter(arrayAdapter,
+				new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						String strName = arrayAdapter.getItem(which);
+						int position = getArguments().getInt("position");
+
+						AlertDialog.Builder builderInner = new AlertDialog.Builder(
+								getActivity());
+						aController.forwardChecklineToAnotherList(position,
+								which, getActivity());
+						builderInner.setMessage(strName);
+						builderInner.setTitle("Your Selected Item is");
+						builderInner.setPositiveButton("Ok",
+								new DialogInterface.OnClickListener() {
+
+									@Override
+									public void onClick(DialogInterface dialog,
+											int which) {
+										dialog.dismiss();
+									}
+								});
+						builderInner.show();
+					}
+				});
+		return builderSingle.create();
+
+	}
+}
