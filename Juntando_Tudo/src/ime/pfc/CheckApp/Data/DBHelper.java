45e1f7e13565ffcf19edb319d7cf1ec9c11e6f98
Wilson Camara Marriel <wilsonmarriel@gmail.com>
2014-05-27 19:52:37 -0300

    Changed Strings to BR

--- Juntando_Tudo/src/ime/pfc/CheckApp/Data/DBHelper.java
+++ Juntando_Tudo/src/ime/pfc/CheckApp/Data/DBHelper.java
@@ -25,7 +25,7 @@ public class DBHelper extends SQLiteOpenHelper {
 	public static final String LOG = "DBHelper";
 
 	// Database Version
-	public static final int DATABASE_VERSION = 2;
+	public static final int DATABASE_VERSION = 3;
 
 	// Database Name
 	public static final String DATABASE_NAME = "GTDManagerDB";
