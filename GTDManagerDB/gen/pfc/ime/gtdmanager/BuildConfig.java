7677e327a2becb37f26d2db2db70192bca704f32
rafaelanastacioalves <rafaelanastacioalves@gmail.com>
2017-10-12 22:56:48 -0300

    refactor: Muda para Gradle

--- GTDManagerDB/gen/pfc/ime/gtdmanager/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package pfc.ime.gtdmanager;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- Juntando_Tudo/gen/com/juntando_tudo/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.juntando_tudo;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
